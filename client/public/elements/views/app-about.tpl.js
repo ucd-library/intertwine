f58fafcc1347ebf9ab98b0990dedf66c2c44e924
Leigh Dragoon <ladragoo@campus-017-066.ucdavis.edu>
2019-11-19 13:24:53 -0800

    Tweaked about page styles a little

--- client/public/elements/views/app-about.tpl.js
+++ client/public/elements/views/app-about.tpl.js
@@ -6,11 +6,26 @@ export default function render() {
   ${sharedStyles}
   <style>
     :host {
-      background-color: pink;
+      background-color: white;
+      display: block;
+    }
+
+    main {
+      padding: 10px;
     }
   </style>
 
   <div class="content">
-    <h1>About IntertWINE</h1>
+    <main>
+      <h1>About IntertWINE</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </main>
   </div>
 `;}
