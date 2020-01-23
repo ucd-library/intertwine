d92340406f99fb4b3f3602426720543bb8725e95
Leigh Dragoon <ladragoo@administrators-MacBook-Pro-2.local>
2020-01-23 08:23:53 -0800

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 PROJECT_ID=digital-ucdavis-edu
 CONTAINER_NAME=intert-wine
+DEPLOYMENT_NAME=leigh-intert-wine
 IMAGE=gcr.io/$PROJECT_ID/$CONTAINER_NAME
 
 gcloud builds submit --tag $IMAGE
 
-gcloud beta run deploy intert-wine \
+gcloud beta run deploy $DEPLOYMENT_NAME \
   --image $IMAGE \
   --platform managed \
   --memory=1Gi \
