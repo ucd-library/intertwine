d6c594dbf73d9522da68f83cf74c4b64d5d48256
Leigh Dragoon <ladragoo@campus-110-083.ucdavis.edu>
2020-02-11 14:14:04 -0800

    Removed obsolete npm call

--- controllers/api/graph.js
+++ controllers/api/graph.js
@@ -1,19 +1,6 @@
 var router  = require('express').Router();
 let fs      = require('fs');
 let path    = require('path');
-let fetch   = require('node-fetch');
-
-const openGeocoder = require('node-open-geocoder');
-
-// This isn't declared as 'async' because it already returns a promise
-function get_location_info (name) {
-  return new Promise((resolve, reject) => {
-    openGeocoder().geocode(name).end((err, res) => {
-      if ( !res ) reject(res);
-      else resolve(res);
-    });
-  });
-}
 
 router.get('/:id', (req, res) => {
   let _data = fs.readFileSync(path.join(__dirname, '..', '..', 'mock', req.params.id + '.json'), 'utf-8');
