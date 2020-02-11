3cad9b06e2dfd806db70ce9d88ade8230e4d0eeb
Leigh Dragoon <ladragoo@campus-110-083.ucdavis.edu>
2020-02-11 14:04:17 -0800

    Added some basic styling to dropdown

--- client/public/elements/app-moments-dropdown.tpl.js
+++ client/public/elements/app-moments-dropdown.tpl.js
@@ -8,6 +8,14 @@ return html`
     :host {
       display: block;
     }
+
+    #moments {
+      padding: 5px 30px 5px 10px;
+      font-size: 1.0rem;
+      border: none;
+      height: 30px;
+      background-color: white;
+    }
   </style>
 
   <select name="moments" id="moments" @change=${e => this.updated(e)}>
