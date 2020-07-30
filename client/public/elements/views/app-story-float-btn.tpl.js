27af9bdeed721babf24300658e49053f6030d1f3
Leigh Dragoon <ladragoon@ucanr.edu>
2020-07-30 12:15:19 -0700

    Correct color of floating btn on Story page

--- client/public/elements/views/app-story-float-btn.tpl.js
+++ client/public/elements/views/app-story-float-btn.tpl.js
@@ -19,7 +19,8 @@ export default function render() {
       border: 0;
       white-space: nowrap;
 
-      background-color: var(--app-color-charcoal);
+      /*background-color: var(--app-color-charcoal);*/
+      background-color: var(--app-color-secondary-blue);
 
       -webkit-box-shadow: -2px 0px 6px 0px rgba(50, 50, 50, 0.3);
       -moz-box-shadow:    -2px 0px 6px 0px rgba(50, 50, 50, 0.3);
@@ -29,9 +30,13 @@ export default function render() {
       opacity: 1;
       transition: visibility 0s .5s, opacity .5s ease;
     }
+
+    .floating-btn:hover {
+      background-color: var(--app-color-charcoal);
+    }
   </style>
 
-  <button class="floating-btn btn secondary-blue-bg">
+  <button class="floating-btn btn">
     <iron-icon class="explore-map-icon" icon="intert-wine-icons:explore"></iron-icon>
     Explore the Map
     <iron-icon class="chevron-right" icon="chevron-right"></iron-icon>
